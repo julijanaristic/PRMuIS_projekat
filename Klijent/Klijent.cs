1d5e5ec6e3afa13d208749cf5c893fb4130f5099
julijanaristic <julijanaristic223@gmail.com>
2024-12-29 16:42:28 +0100

    zadatak 1

---
+++ Klijent/Klijent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Klijent
+{
+    public class Klijent
+    {
+        static void Main(string[] args)
+        {
+            Socket klijentSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint klijentEP = new IPEndPoint(IPAddress.Parse("192.168.56.1"), 12345);
+
+            while (true)
+            {
+                try
+                {
+                    string imeIgraca;
+                    do
+                    {
+                        Console.Write("Unesite vaše ime/nadimak: ");
+                        imeIgraca = Console.ReadLine()?.Trim();
+
+                        if (string.IsNullOrEmpty(imeIgraca))
+                        {
+                            Console.WriteLine("Ime nije unijeto! Morate unijeti ime.");
+                        }
+
+                    } while (string.IsNullOrEmpty(imeIgraca));
+
+                    Console.WriteLine("Izaberite igre koje želite igrati [sl, sk, kzz] (odvojene zarezima): ");
+                    string listaIgara = Console.ReadLine()?.Trim();
+
+                    string poruka = $"PRIJAVA: {imeIgraca}, {listaIgara}";
+                    byte[] bajti = Encoding.UTF8.GetBytes(poruka);
+
+                    klijentSocket.SendTo(bajti, klijentEP);
+                    Console.WriteLine("Uspješno poslata prijava.");
+
+                    /*byte[] bafer = new byte[1024];
+                    EndPoint primljeniEP = new IPEndPoint(IPAddress.Any, 0);
+                    int primljeniBajti = klijentSocket.ReceiveFrom(bafer, ref primljeniEP);
+
+                    string odgovor = Encoding.UTF8.GetString(bafer, 0, primljeniBajti);
+                    Console.WriteLine($"Odgovor od servera: {odgovor}");*/
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine($"Greška prilikom slanja poruke (klijent): {ex.Message}");
+                }
+            }
+
+            klijentSocket.Close();
+        }
+    }
+}
---
+++ Server/Server/Server.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 12345);
+            serverSocket.Bind(serverEP);
+
+            Console.WriteLine("Server pokrenut. Čekanje na prijave igrača preko UDP-a.");
+
+            while (true)
+            {
+                byte[] bafer = new byte[1024];
+                EndPoint klijentEP = new IPEndPoint(IPAddress.Any, 0);
+                try
+                {
+                    int primljeniBajti = serverSocket.ReceiveFrom(bafer, ref klijentEP);
+                    string primljenaPoruka = Encoding.UTF8.GetString(bafer, 0, primljeniBajti);
+
+                    Console.WriteLine($"Primljena poruka: {primljenaPoruka}");
+
+                    if(primljenaPoruka.StartsWith("PRIJAVA: "))
+                    {
+                        string[] dijelovi = primljenaPoruka.Substring(9).Split(new char[] {','}, 2);
+                        if(dijelovi.Length == 2)
+                        {
+                            string imeIgraca = dijelovi[0].Trim();
+                            string listaIgara = dijelovi[1].Trim();
+
+                            if (ValidacijaIgara(listaIgara))
+                            {
+                                Console.WriteLine($"Prijava uspješna\nIme igrača: {imeIgraca}\nLista igara: {listaIgara}");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Nevalidna lista igara.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nepravilno formatirana prijava.");
+                        }
+
+
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine($"Greška prilikom prijema podataka na serveru: {ex.Message}");
+                }
+            }
+
+            serverSocket.Close();
+        }
+
+        private static bool ValidacijaIgara(string listaIgara)
+        {
+            string[] validneIgre = { "sl", "sk", "kzz" };
+            string[] igre = listaIgara.Split(',');
+            foreach (var igra in igre)
+            {
+                if (!validneIgre.Contains(igra.Trim()))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
