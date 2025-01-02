927d7faa57c5a67afcd4650b154c33af1151e273
julijanaristic <julijanaristic223@gmail.com>
2025-01-02 18:40:09 +0100

    zadatak 3 zapocet

---
+++ Server/Igrac.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Igrac
+{
+	public int BrojIgraca { get; set; }
+	public string Ime {  get; set; }
+	public int[] BrojPoenaPoIgrama { get; set; }
+
+	public Igrac(int brojIgraca, string ime, int brojIgara)
+	{
+		BrojIgraca = brojIgraca;
+		Ime = ime;
+		BrojPoenaPoIgrama = new int[brojIgara];
+	}
+
+
+
+}
